2c0e82fc8ab73f28cd940c1bef5219f36517061d
Furkan <info.furkanoz@gmail.com>
2022-07-25 23:35:49 +0300

    Footer Added

---
+++ src/Components/SideBar/RightSideBar/Footer/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <div className="navbar">
+                <div className="navbar-item link">Terms of Service</div>
+                <div className="navbar-item link">Privacy Policy</div>
+                <div className="navbar-item link">Cookie Policy</div>
+                <div className="navbar-item link">Imprint</div>
+                <div className="navbar-item link">Accessibility</div>
+                <div className="navbar-item link">Ads info</div>
+                <div className="navbar-item link">More...</div>
+            <div className="navbar-item">© 2022 Twitter, Inc.</div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
