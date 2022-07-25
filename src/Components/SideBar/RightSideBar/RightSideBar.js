c0a8ce71c85cc76aa0d764d1d7a7c0be30f19896
Furkan <info.furkanoz@gmail.com>
2022-07-25 23:36:08 +0300

    Right Sidebar Added

---
+++ src/Components/SideBar/RightSideBar/RightSideBar.js
@@ -0,0 +1,18 @@
+import SearchBar from "../../SearchBar/SearchBar";
+import TrendsForYou from "../../Trends/TrendsForYou/TrendsForYou";
+import WhoFollow from "../../WhoFollow/WhoFollow";
+import Footer from "./Footer/Footer";
+
+const RightSideBar = () => {
+    
+    return (
+        <>
+        <SearchBar/>
+        <TrendsForYou/>
+        <WhoFollow/>
+        <Footer/>
+        </>
+    )
+}
+
+export default RightSideBar;
