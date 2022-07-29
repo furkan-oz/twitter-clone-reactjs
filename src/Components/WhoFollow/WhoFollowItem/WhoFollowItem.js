fa072401d691621899d6abf9debaffa5189a89d7
Furkan <info.furkanoz@gmail.com>
2022-07-29 23:55:20 +0300

    Who Follow Item Updated

--- src/Components/WhoFollow/WhoFollowItem/WhoFollowItem.js
+++ src/Components/WhoFollow/WhoFollowItem/WhoFollowItem.js
@@ -3,15 +3,15 @@ import './WhoFollowItem.css'
 
 const WhoFollowItem = () => {
     return(
-        <div className="WhoFollowItem">
-            <div className="ProfilePhoto">
-                <img src="https://pbs.twimg.com/profile_images/1408089447455891461/hwvI8tM1_400x400.jpg" alt="" />
+        <div className="who-follow-item d-flex align-items-center w-100 m-auto">
+            <div className="follow-profile-photo mt-2 mb-2 ms-3">
+                <img className='rounded-circle' src="https://pbs.twimg.com/profile_images/1408089447455891461/hwvI8tM1_400x400.jpg" alt="" />
             </div>
-            <div className="ProfileInfo">
-                <div className="ProfileName">Windows</div>
-                <div className="ProfileNick">@Windows</div>
+            <div className="d-flex flex-column ms-1">
+                <div className="follow-profile-name fw-semibold">Windows</div>
+                <div className="follow-profile-nick text-secondary">@Windows</div>
             </div>
-            <div className="FollowButton">
+            <div className="ms-auto me-3">
                 <Buttons buttonType={"FollowBtn"} buttonText={"Follow"}/>
             </div>
         </div>
