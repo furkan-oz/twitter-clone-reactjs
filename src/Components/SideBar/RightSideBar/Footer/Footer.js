caa34a517aa8526df2a972f88bdb3bca67eb6f2c
Furkan <info.furkanoz@gmail.com>
2022-07-29 23:54:00 +0300

    Footer Updated

--- src/Components/SideBar/RightSideBar/Footer/Footer.js
+++ src/Components/SideBar/RightSideBar/Footer/Footer.js
@@ -2,16 +2,16 @@ import './Footer.css';
 
 const Footer = () => {
     return(
-        <div className="footer">
-            <div className="navbar">
-                <div className="navbar-item link">Terms of Service</div>
-                <div className="navbar-item link">Privacy Policy</div>
-                <div className="navbar-item link">Cookie Policy</div>
-                <div className="navbar-item link">Imprint</div>
-                <div className="navbar-item link">Accessibility</div>
-                <div className="navbar-item link">Ads info</div>
-                <div className="navbar-item link">More...</div>
-            <div className="navbar-item">© 2022 Twitter, Inc.</div>
+        <div className="w-75 mt-3 mb-3 mx-auto">
+            <div className="navbar d-flex justify-content-start">
+                <div className="navbar-item pe-2 text-secondary link">Terms of Service</div>
+                <div className="navbar-item pe-2 text-secondary link">Privacy Policy</div>
+                <div className="navbar-item pe-2 text-secondary link">Cookie Policy</div>
+                <div className="navbar-item pe-2 text-secondary link">Imprint</div>
+                <div className="navbar-item pe-2 text-secondary link">Accessibility</div>
+                <div className="navbar-item pe-2 text-secondary link">Ads info</div>
+                <div className="navbar-item pe-2 text-secondary link">More...</div>
+            <div className="navbar-item text-secondary">© 2022 Twitter, Inc.</div>
             </div>
         </div>
     )
