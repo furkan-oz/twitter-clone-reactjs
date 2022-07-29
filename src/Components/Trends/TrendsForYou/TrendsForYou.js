93930d77baa52596268fc48b65ad35538c480e8c
Furkan <info.furkanoz@gmail.com>
2022-07-29 23:54:39 +0300

    Trends For You Updated

--- src/Components/Trends/TrendsForYou/TrendsForYou.js
+++ src/Components/Trends/TrendsForYou/TrendsForYou.js
@@ -3,11 +3,11 @@ import TrendItem from './TrendItem/TrendItem';
 import ShowMore from './ShowMore/ShowMore';
 const TrendsForYou = () => {
     return(
-        <div className="TrendsForYou">
-            <div className="TrendsTitle">
-                <h2>Trends for you</h2>
+        <div className="trends-for-you d-flex flex-column bg-light mt-2 mx-auto">
+            <div>
+                <h2 className='fs-5 fw-bold mt-3 mb-3 ms-3'>Trends for you</h2>
             </div>
-            <div className='TrendItems'>
+            <div>
                 <TrendItem/>
                 <TrendItem/>
                 <TrendItem/>
