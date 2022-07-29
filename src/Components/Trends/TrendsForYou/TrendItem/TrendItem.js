25f37081dd3a1041b768e64ed8343673aabbc403
Furkan <info.furkanoz@gmail.com>
2022-07-29 23:54:21 +0300

    Trend Item Updated

--- src/Components/Trends/TrendsForYou/TrendItem/TrendItem.js
+++ src/Components/Trends/TrendsForYou/TrendItem/TrendItem.js
@@ -2,16 +2,16 @@ import Icons from "../../../Icons/Icons";
 import './TrendItem.css'
 const TrendItem = () => {
     return (
-        <div className="TrendItem">
-            <div className="IconItem">
-                <div className="MoreIcon">
+        <div className="trend-item position-relative p-3">
+            <div className="icon-item rounded-pill position-absolute">
+                <div className="more-icon m-auto">
                     <Icons iconName={"noCircleMore"} />
                 </div>
             </div>
-            <div className="TextItem">
-                <div className="AboutItem">Music · Trending</div>
-                <div className="NameItem">#Eminem</div>
-                <div className="TweetCount">13.3K Tweets</div>
+            <div className="d-flex flex-column">
+                <div className="about-item text-secondary">Music · Trending</div>
+                <div className="name-item fw-bold">#Eminem</div>
+                <div className="tweet-count text-secondary">13.3K Tweets</div>
             </div>
         </div>
     )
