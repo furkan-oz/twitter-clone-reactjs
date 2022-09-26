43134a42b289b8cf4a73b1f5587b1a0ac64298cb
Furkan <info.furkanoz@gmail.com>
2022-09-26 21:35:50 +0300

    TrendsForYou updated

--- src/Components/Trends/TrendsForYou/TrendsForYou.js
+++ src/Components/Trends/TrendsForYou/TrendsForYou.js
@@ -1,9 +1,9 @@
 import './TrendsForYou.css'
 import TrendItem from './TrendItem/TrendItem';
-import ShowMore from './ShowMore/ShowMore';
-const TrendsForYou = () => {
+import ShowMore from '../../ShowMore/ShowMore';
+const TrendsForYou = (props) => {
     return(
-        <div className="trends-for-you d-flex flex-column bg-light mt-2 mx-auto">
+        <div style={{width: props.width , borderRadius: props.borderRadius}} className="trends-for-you d-flex flex-column bg-light mt-2 mx-auto">
             <div>
                 <h2 className='fs-5 fw-bold mt-3 mb-3 ms-3'>Trends for you</h2>
             </div>
