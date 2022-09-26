b4e6a4fcd5041490e69c135425dbbca133e15eb9
Furkan <info.furkanoz@gmail.com>
2022-09-26 21:38:45 +0300

    Explore added

---
+++ src/Pages/Explore/Explore.js
@@ -0,0 +1,32 @@
+import Icons from '../../Components/Icons/Icons';
+import SearchBar from '../../Components/SearchBar/SearchBar';
+import TrendsForYou from '../../Components/Trends/TrendsForYou/TrendsForYou';
+import "./Explore.css";
+const Explore = () => {
+    document.title = "Explore / Twitter";
+
+    return(
+        <div className="border-start border-end h-100 w-100">
+            <div className='d-flex flex-row sticky-top bg-white'>
+                <div className='w-100'>
+                    <SearchBar/>
+                </div>
+                <div className="settings-icon-item mt-auto mb-auto rounded-pill">
+                    <div className="settings-icon m-auto">
+                        <Icons iconName={"settings"}/>
+                    </div>
+                </div>
+            </div>
+            <div className='position-relative'>
+                <img className='explore-image w-100' src="https://pbs.twimg.com/semantic_core_img/1255910073831788550/xm0-QtpW?format=jpg&name=small" alt="" />
+                <div className='position-absolute bottom-0 start-0 mb-4 pb-3 ms-3 text-white '>Event · LIVE</div>
+                <div className='position-absolute bottom-0 start-0 mb-2 ms-3 text-white fw-bold fs-4'>Updates on the COVID-19 situation in Turkey</div>
+            </div>
+            <div className='border-top border-bottom'>
+                <TrendsForYou borderRadius={"0px"} width={"100%"}/>
+            </div>
+        </div>
+    )
+}
+
+export default Explore;
