ffdd6aacb5fac5d961faa98fcb1d19f28b17887c
Furkan <info.furkanoz@gmail.com>
2022-09-26 21:32:10 +0300

    New icons added

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -16,25 +16,37 @@ import PollIcon from "./PollIcon";
 import EmoteIcon from "./EmoteIcon";
 import ScheduleIcon from "./ScheduleIcon";
 import LocationIcon from "./LocationIcon";
+import CommentIcon from "./CommentIcon";
+import RetweetIcon from "./RetweetIcon";
+import LikeIcon from "./LikeIcon";
+import ShareIcon from "./ShareIcon";
+import SettingsIcon from "./SettingsIcon";
+import MessageIcon from "./MessageIcon";
+import BackIcon from "./BackIcon";
+import ListIcon from "./ListIcon";
+import JoinDateIcon from "./JoinDateIcon";
+import SideBarButtonIcon from "./SideBarButtonIcon";
+
+
 
 const Icons = (props) => {
 
     switch (props.iconName)
     {
         case 'home':
-            return <HomeIcon active={false}/>
+            return <HomeIcon active={props.active}/>
         case 'explore':
-            return <ExploreIcon active={false}/>
+            return <ExploreIcon active={props.active}/>
         case 'notifications':
-            return <NotificationsIcon active={false}/>
+            return <NotificationsIcon active={props.active}/>
         case 'messages':
-            return <MessagesIcon active={false}/>
+            return <MessagesIcon active={props.active}/>
         case 'bookmarks':
-            return <BookmarksIcon active={false}/>
+            return <BookmarksIcon active={props.active}/>
         case 'lists':
-            return <ListsIcon active={false}/>
+            return <ListsIcon active={props.active}/>
         case 'profile':
-            return <ProfileIcon active={false}/>
+            return <ProfileIcon active={props.active}/>
         case 'more':
             return <MoreIcon/>
         case 'search':
@@ -57,6 +69,26 @@ const Icons = (props) => {
             return <ScheduleIcon/>
         case 'location':
             return <LocationIcon/>
+        case 'comment':
+            return <CommentIcon/>
+        case 'retweet':
+            return <RetweetIcon/>
+        case 'like':
+            return <LikeIcon/>
+        case 'share':
+            return <ShareIcon/>
+        case 'settings':
+            return <SettingsIcon/>
+        case 'message':
+            return <MessageIcon/>
+        case 'back':
+            return <BackIcon/>
+        case 'list':
+            return <ListIcon/>
+        case 'date':
+            return <JoinDateIcon/>
+        case 'sidebarButton':
+            return <SideBarButtonIcon/>
         default:
             return null
         }
