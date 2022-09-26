8014f927b047b66d6fabbe1b787f55968ca81987
Furkan <info.furkanoz@gmail.com>
2022-09-26 21:33:35 +0300

    RightSideBar updated

--- src/Components/SideBar/RightSideBar/RightSideBar.js
+++ src/Components/SideBar/RightSideBar/RightSideBar.js
@@ -2,16 +2,16 @@ import SearchBar from "../../SearchBar/SearchBar";
 import TrendsForYou from "../../Trends/TrendsForYou/TrendsForYou";
 import WhoFollow from "../../WhoFollow/WhoFollow";
 import Footer from "./Footer/Footer";
+import {useLocation} from "react-router-dom";
+import './RightSideBar.css';
 
 const RightSideBar = () => {
-    
+    const location = useLocation();
+
     return (
-        <>
-        <SearchBar/>
-        <TrendsForYou/>
-        <WhoFollow/>
-        <Footer/>
-        </>
+        <div className="right-side-bar">
+        {location.pathname === "/explore" ? <> <WhoFollow/> <Footer/> </> :<> <SearchBar/> <TrendsForYou/> <WhoFollow/> <Footer/> </>}
+        </div>
     )
 }
 
