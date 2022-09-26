7948062f94fc4b4c71d886e7e9681d2feb080007
Furkan <info.furkanoz@gmail.com>
2022-09-26 21:37:45 +0300

    WhoFollow updated

--- src/Components/WhoFollow/WhoFollow.js
+++ src/Components/WhoFollow/WhoFollow.js
@@ -1,15 +1,15 @@
 import WhoFollowItem from "./WhoFollowItem/WhoFollowItem";
 import './WhoFollow.css'
-import ShowMore from "../Trends/TrendsForYou/ShowMore/ShowMore";
+import ShowMore from "../ShowMore/ShowMore";
 
-const WhoFollow = () => {
+const WhoFollow = (props) => {
     return(
-        <div className="who-follow d-flex flex-column bg-light mx-auto mt-2">
+        <div className="who-follow d-flex flex-column mx-auto mt-2" style={{background: props.background , width:props.width}}>
             <h2 className="fs-5 fw-bold mt-3 mb-3 ms-3">Who to follow</h2>
             <WhoFollowItem/>
             <WhoFollowItem/>
             <WhoFollowItem/>
-            <ShowMore/>
+            <ShowMore borderRadius={props.borderRadius}/>
         </div>
     )
 }
