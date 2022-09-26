bd7d8af1cd908233b2f073e4518b109b761a4038
Furkan <info.furkanoz@gmail.com>
2022-09-26 21:33:05 +0300

    Show more added

---
+++ src/Components/ShowMore/ShowMore.js
@@ -0,0 +1,11 @@
+import './ShowMore.css'
+
+const ShowMore = (props) => {
+    return(
+        <div style={{borderRadius: props.borderRadius}} className="show-more p-3">
+            Show more
+        </div>
+    )
+}
+
+export default ShowMore;
