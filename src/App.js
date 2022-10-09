8784888292bf0c05155d563f5934a827f749c826
Furkan Öz <info.furkanoz@gmail.com>
2022-10-09 21:00:12 +0300

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, NavLink, Route, Routes, useLocation } from "react-router-dom";
+import Icons from "./Components/Icons/Icons.js";
 import LeftSideBar from "./Components/SideBar/LeftSideBar/LeftSideBar.js";
 import RightSideBar from "./Components/SideBar/RightSideBar/RightSideBar.js";
 import Bookmarks from "./Pages/Bookmarks/Bookmarks.js";
@@ -11,6 +12,7 @@ import Profile from "./Pages/Profile/Profile.js";
 
 const App = () => {
   const location = useLocation();
+  
 
   return (
     <div className="container">
@@ -34,6 +36,56 @@ const App = () => {
           </Routes>
         </div>
         {location.pathname === "/messages" ? null : <div className="col-4"><RightSideBar/></div>}
+      </div>
+      <div className="row sticky-bottom bg-white">
+        <div className="col-12">
+          <div className="navbar-bottom d-flex border-top text-center">
+            <div className="navbar-bottom-item">
+                <NavLink to="/home">
+                    <div>
+                      <div className="nav-item d-inline-block rounded-pill p-2 m-1">
+                        <div className="nav-icon d-inline-block">
+                          <Icons active={location.pathname === "/home" ? true : false} iconName={"home"}/>
+                        </div>
+                      </div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className="navbar-bottom-item">
+                <NavLink to="/explore" >
+                    <div>
+                      <div className="nav-item d-inline-block rounded-pill p-2 m-1">
+                        <div className="nav-icon d-inline-block">
+                          <Icons active={location.pathname === "/explore" ? true : false} iconName={"search"}/>
+                        </div>
+                      </div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className="navbar-bottom-item">
+                <NavLink to="/notifications">
+                    <div>
+                      <div className="nav-item d-inline-block rounded-pill p-2 m-1">
+                        <div className="nav-icon d-inline-block">
+                          <Icons active={location.pathname === "/notifications" ? true : (location.pathname === "/notifications/mentions" ? true : false)} iconName={"notifications"}/>
+                        </div>
+                      </div>
+                    </div>
+                </NavLink>
+            </div>
+            <div className="navbar-bottom-item">
+                <NavLink to="/messages">
+                    <div>
+                      <div className="nav-item d-inline-block rounded-pill p-2 m-1">
+                        <div className="nav-icon d-inline-block">
+                          <Icons active={location.pathname === "/messages" ? true : false} iconName={"messages"}/>
+                        </div>
+                      </div>
+                    </div>
+                </NavLink>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
