80ab96cfa6cb6272aa21290aebf403642c50099b
Furkan Öz <info.furkanoz@gmail.com>
2022-10-09 21:02:57 +0300

    Updated WhoFollowItem

--- src/Components/WhoFollow/WhoFollowItem/WhoFollowItem.js
+++ src/Components/WhoFollow/WhoFollowItem/WhoFollowItem.js
@@ -1,15 +1,35 @@
+import { useEffect, useState } from 'react';
 import Buttons from '../../Button/Buttons'
 import './WhoFollowItem.css'
 
 const WhoFollowItem = () => {
+
+    const [names,setNames] = useState();
+    const [lastName,setLastNames] = useState();
+    const [picture,setPicture] = useState();
+    const [username,setUsername] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://randomuser.me/api/')
+            const data = await response.json();
+            setNames(data.results[0].name.first);
+            setLastNames(data.results[0].name.last);
+            setUsername(data.results[0].login.username);
+            setPicture(data.results[0].picture.large);
+            
+        }
+        fetchData();
+      },[]);
+
     return(
         <div className="who-follow-item d-flex align-items-center w-100 m-auto">
             <div className="follow-profile-photo mt-2 mb-2 ms-3">
-                <img className='rounded-circle' src="https://pbs.twimg.com/profile_images/1408089447455891461/hwvI8tM1_400x400.jpg" alt="" />
+                <img className='rounded-circle' src={picture} alt="" />
             </div>
             <div className="d-flex flex-column ms-1">
-                <div className="follow-profile-name fw-semibold">Windows</div>
-                <div className="follow-profile-nick text-secondary">@Windows</div>
+                <div className="follow-profile-name fw-semibold">{`${names} ${lastName}`}</div>
+                <div className="follow-profile-nick text-secondary">@{username}</div>
             </div>
             <div className="ms-auto me-3">
                 <Buttons buttonType={"FollowBtn"} buttonText={"Follow"}/>
