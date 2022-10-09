565fe24731ca0e1bcfc79d2c38e913f97d3e8411
Furkan Öz <info.furkanoz@gmail.com>
2022-10-09 21:01:26 +0300

    Updated NewListItem

--- src/Components/NewList/NewListItem/NewListItem.js
+++ src/Components/NewList/NewListItem/NewListItem.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import Buttons from "../../Button/Buttons";
 import './NewListItem.css';
 
 const NewListItem = () => {
+
+    const [names,setNames] = useState();
+    const [lastName,setLastNames] = useState();
+    const [username,setUsername] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://randomuser.me/api/')
+            const data = await response.json();
+            setNames(data.results[0].name.first);
+            setLastNames(data.results[0].name.last);
+            setUsername(data.results[0].login.username);            
+        }
+        fetchData();
+      },[]);
+
     return(
             <div className="list-item d-flex">
                 <div className="list-item-image">
@@ -10,8 +27,8 @@ const NewListItem = () => {
                 <div className="mt-3 flex-grow-1">
                     <div className="fw-semibold">Kultur Sanat</div>
                     <div className="d-flex">
-                        <div className="list-item-text fw-bold">Berk Bogazkesenli</div>
-                        <div className="list-item-text text-secondary ms-1">@turanberkb</div>
+                        <div className="list-item-text fw-bold">{`${names} ${lastName}`}</div>
+                        <div className="list-item-text text-secondary ms-1">@{username}</div>
                     </div>
                 </div>
                 <div className="mt-3 me-3">
